b7cdd8aa4096e3026330c66a6acb313c10fd70d8
Trine <trla0001@stud.kea.dk>
2024-02-01 12:42:18 +0100

    done med alle øvelser

--- 3.js
+++
@@ -1,5 +0,0 @@
-/************** Exercise 3 ****************/
-
-/* 11 istedet for 10 i mat.floor*/
-
-/* confirm er pop up boks, man kan også bruge alert. Er ikke brugervenligt, ville lave sin egen boks*/
